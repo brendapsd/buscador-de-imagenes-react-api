932a38f254633ca4cf0645465b1146f3d3183e8b
brendapsd <brenda.sd.15@gmail.com>
2019-08-28 12:57:45 -0500

    Updates

---
+++ precache-manifest.19857317556e73ad604f50ac71d56ffa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8c054992307d76c79abb9e7dfe073c",
+    "url": "/brendapsd/buscador-de-imagenes-react-api/index.html"
+  },
+  {
+    "revision": "be5f9798c2978306549f",
+    "url": "/brendapsd/buscador-de-imagenes-react-api/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "16b106343b74aaf6ffc6",
+    "url": "/brendapsd/buscador-de-imagenes-react-api/static/js/2.89ffd26a.chunk.js"
+  },
+  {
+    "revision": "be5f9798c2978306549f",
+    "url": "/brendapsd/buscador-de-imagenes-react-api/static/js/main.1774fef6.chunk.js"
+  },
+  {
+    "revision": "f686ad2b448dea827b93",
+    "url": "/brendapsd/buscador-de-imagenes-react-api/static/js/runtime~main.bef553e6.js"
+  }
+]);
